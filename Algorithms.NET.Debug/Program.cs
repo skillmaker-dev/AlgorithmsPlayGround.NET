d9d3a94af81283926648899aa5d6a615a61fca45
skillmaker <anas.chahid777@gmail.com>
2022-08-25 22:26:14 +0100

    Add project files.

---
+++ Algorithms.NET.Debug/Program.cs
@@ -0,0 +1,16 @@
+﻿List<double> unsortedAsc = new() { 0,1,2,3,4,5 };
+List<double> unsortedDesc = new() { 5,4,3,2,1,0 };
+
+var sortedDesc = Algorithms.NET.BubbleSort.SortDescending(unsortedAsc);
+var sortedAsc = Algorithms.NET.BubbleSort.SortAscending(unsortedDesc);
+
+
+foreach(var item in sortedDesc)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine();
+foreach (var item in sortedAsc)
+{
+    Console.WriteLine(item);
+}
---
+++ Algorithms.NET/BubbleSort.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.NET
+{
+    public static class BubbleSort
+    {
+        public static IEnumerable<double> SortDescending(List<double> list)
+        {
+            var sortedList = list;
+            bool isSorted;
+            for(int i = 0;i <sortedList.Count;i++)
+            {
+                isSorted = true;
+                for(int j = 1;j <sortedList.Count - i;j++)
+                    if(sortedList[j-1] < sortedList[j])
+                    {
+                        (sortedList[j-1],sortedList[j]) = (sortedList[j],sortedList[j-1]);
+                        isSorted = false;
+                    }
+
+                if(isSorted)
+                    return sortedList;
+            }
+
+            return sortedList;
+        }
+
+        public static IEnumerable<double> SortAscending(List<double> list)
+        {
+            var sortedList = list;
+            bool isSorted;
+
+            for(int i = 0;i <sortedList.Count;i++)
+            {
+                isSorted = true;
+                for(int j = 1;j <sortedList.Count - i;j++)
+                    if(sortedList[j-1] > sortedList[j])
+                    {
+                        (sortedList[j - 1], sortedList[j]) = (sortedList[j], sortedList[j - 1]);
+                        isSorted = false;
+                    }
+
+
+                if (isSorted)
+                    return sortedList;
+            }
+
+            return sortedList;
+        }
+    }
+}
