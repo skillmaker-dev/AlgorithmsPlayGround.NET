d44388dc88c63b8d3018f2fdc3ca2c02f98b6ef9
skillmaker <anas.chahid777@gmail.com>
2022-08-25 22:49:51 +0100

    code reusability

--- Algorithms.NET/BubbleSort/BubbleSort.cs
+++ Algorithms.NET/BubbleSort/BubbleSort.cs
@@ -5,41 +5,55 @@ namespace Algorithms.NET.BubbleSort
 {
     public static class BubbleSort
     {
+        /// <summary>
+        /// Sorting a list of numbers in descending order using BubbleSort algorithm.
+        /// </summary>
+        /// <param name="list">List of numbers to sort</param>
+        /// <returns>Sorted list in descending order.</returns>
         public static IEnumerable<double> SortDescending(List<double> list)
         {
-            var sortedList = list;
-            bool isSorted;
-            for(int i = 0;i <sortedList.Count;i++)
-            {
-                isSorted = true;
-                for(int j = 1;j <sortedList.Count - i;j++)
-                    if(sortedList[j-1] < sortedList[j])
-                    {
-                        (sortedList[j-1],sortedList[j]) = (sortedList[j],sortedList[j-1]);
-                        isSorted = false;
-                    }
-
-                if(isSorted)
-                    return sortedList;
-            }
-
-            return sortedList;
+            return Sort(list, true);
         }
 
+        /// <summary>
+        /// Sorting a list of numbers in ascending order using BubbleSort algorithm.
+        /// </summary>
+        /// <param name="list">List of numbers to sort</param>
+        /// <returns>Sorted list in ascending order.</returns>
         public static IEnumerable<double> SortAscending(List<double> list)
+        {
+            return Sort(list, false);
+        }
+
+
+        /// <summary>
+        /// Sorting a list according ascending or descending
+        /// </summary>
+        /// <param name="list">List of numbers to sort</param>
+        /// <param name="sortDescending">Boolean value specifying wheter sorting should be done descending</param>
+        /// <returns>A sorted list</returns>
+        private static IEnumerable<double> Sort(List<double> list, bool sortDescending)
         {
             var sortedList = list;
             bool isSorted;
 
-            for(int i = 0;i <sortedList.Count;i++)
+            for (int i = 0; i < sortedList.Count; i++)
             {
                 isSorted = true;
-                for(int j = 1;j <sortedList.Count - i;j++)
-                    if(sortedList[j-1] > sortedList[j])
+                for (int j = 1; j < sortedList.Count - i; j++)
+                {
+                    if (sortDescending && sortedList[j - 1] < sortedList[j])
                     {
                         (sortedList[j - 1], sortedList[j]) = (sortedList[j], sortedList[j - 1]);
                         isSorted = false;
                     }
+                    else if(!sortDescending && sortedList[j - 1] > sortedList[j])
+                    {
+                        (sortedList[j - 1], sortedList[j]) = (sortedList[j], sortedList[j - 1]);
+                        isSorted = false;
+                    }
+                }
+
 
 
                 if (isSorted)
