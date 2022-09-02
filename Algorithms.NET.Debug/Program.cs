4f1717f10106c970c98de5b593201a33ce81994f
skillmaker <anas.chahid777@gmail.com>
2022-09-02 21:02:00 +0100

    cleaned program.cs

--- Algorithms.NET.Debug/Program.cs
+++ Algorithms.NET.Debug/Program.cs
@@ -3,40 +3,41 @@ using BenchmarkDotNet.Running;
 
 List<double> unsortedAsc = new() { 0, 1, 2, 3, 4, 5 };
 List<double> unsortedDesc = new() { 5, 4, 3, 2, 1, 0 };
-List<double> unsortedRand = new() { 8, 9, 3, 3, 100000, 1 };
-List<double> unsorted01 = new() { .8, .9, .3, .3, .1,0 };
+List<double> unsortedRand = new() { 8, 9, 3, 3, 7, 1 };
+List<double> unsortedLessThan1 = new() { .8, .9, .3, .3, .1,0 };
 
-//var sortedDesc = Algorithms.NET.Sorting.MergeSort.MergeSortAlgorithm.SortDescending(unsortedAsc);
-//var sortedAsc = Algorithms.NET.Sorting.BucketSort.BucketSortAlgorithm.SortAscending(unsorted01);
-//var sortedDesc = Algorithms.NET.Sorting.BucketSort.BucketSortAlgorithm.SortDescending(unsorted01);
-//var sortedDesc = Algorithms.NET.Sorting.CountingSort.CountingSortAlgorithm.SortDescending(unsortedRand.ToArray());
-//Console.WriteLine(Algorithms.NET.Searching.LinearSearch.LinearSearchAlgorithm.Search(4, unsortedAsc));
-//Console.WriteLine(Algorithms.NET.Searching.BinarySearch.BinarySearchAlgorithm.SearchRecursive(0, unsortedAsc));
-//Console.WriteLine(Algorithms.NET.Searching.BinarySearch.BinarySearchAlgorithm.SearchIterative(0, unsortedAsc));
+var sortedDesc = Algorithms.NET.Sorting.MergeSort.MergeSortAlgorithm.SortDescending(unsortedAsc);
+var sortedAsc = Algorithms.NET.Sorting.BucketSort.BucketSortAlgorithm.SortAscending(unsortedLessThan1);
+
+
+Console.WriteLine(Algorithms.NET.Searching.LinearSearch.LinearSearchAlgorithm.Search(4, unsortedAsc));
+Console.WriteLine();
+Console.WriteLine(Algorithms.NET.Searching.BinarySearch.BinarySearchAlgorithm.SearchRecursive(0, unsortedAsc));
+Console.WriteLine();
+Console.WriteLine(Algorithms.NET.Searching.BinarySearch.BinarySearchAlgorithm.SearchIterative(0, unsortedAsc));
+Console.WriteLine();
 
 Console.WriteLine(Algorithms.NET.Searching.TernarySearch.TernarySearchAlgorithm.Search(8, unsortedAsc));
+Console.WriteLine();
 Console.WriteLine(Algorithms.NET.Searching.JumpSearch.JumpSearchAlgorithm.Search(0, unsortedAsc));
+Console.WriteLine();
 Console.WriteLine(Algorithms.NET.Searching.ExponentialSearch.ExponentialSearchAlgorithm.Search(5, unsortedAsc));
+Console.WriteLine();
 
-//foreach (var item in sortedDesc)
-//{
-//    Console.WriteLine(item);
-//}
-//Console.WriteLine();
-//foreach (var item in sortedAsc)
-//{
-//    Console.WriteLine(item);
-//}
+foreach (var item in sortedDesc)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine();
+foreach (var item in sortedAsc)
+{
+    Console.WriteLine(item);
+}
 
-//List<double> _unsortedAsc = new();
-//var rand = new Random();
-//for (int i = 0; i < 1000000; i++)
-//{
-//    _unsortedAsc.Add(rand.Next(1000000));
-//}
+
 
 //var sortingBenchmarkSummary = BenchmarkRunner.Run<SortingBenchmark>();
-var searchingBenchmarkSummary = BenchmarkRunner.Run<SearchingBenchmark>();
+//var searchingBenchmarkSummary = BenchmarkRunner.Run<SearchingBenchmark>();
 
 
 
